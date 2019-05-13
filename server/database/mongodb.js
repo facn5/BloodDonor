54d05de352bb7330b1fb86510a6aa16153d39ab9
majdya <majd.ya.1995@gmail.com>
2019-05-13 15:55:18 +0300

    moved mongodb to server/database file Related #14

--- server/database/mongodb.js
+++ server/database/mongodb.js
@@ -1,16 +1,12 @@
-// CRUD
-
-const { MongoClient } = require("mongodb");
-
-// eslint-disable-next-line no-multi-spaces
-const { mongoURI } = require("./keys_dev");
+const { MongoClient } = require('mongodb');
+const { mongoURI } = require('../../keys_dev');
 
 // find all in a colletion
 const findAllIn = (colName, query) => {
   const client = new MongoClient(mongoURI, { useNewUrlParser: true });
-  client.connect(err => {
+  client.connect((err) => {
     if (err) throw err;
-    const db = client.db("blooddonor");
+    const db = client.db('blooddonor');
     db.collection(colName)
       .find(query)
       .toArray((error, result) => {
@@ -21,14 +17,14 @@ const findAllIn = (colName, query) => {
   });
 };
 
-findAllIn("users", { name: "majd" });
+findAllIn('users', { name: 'majd' });
 
 // find one in a colletion
 const findOneIn = (colName, query) => {
   const client = new MongoClient(mongoURI, { useNewUrlParser: true });
-  client.connect(err => {
+  client.connect((err) => {
     if (err) throw err;
-    const col = client.db("blooddonor");
+    const col = client.db('blooddonor');
     col.collection(colName).findOne(query, (error, res) => {
       if (error) console.log(error);
       console.log(res);
@@ -37,36 +33,36 @@ const findOneIn = (colName, query) => {
   });
 };
 
-findOneIn("users", { name: "majd" });
+findOneIn('users', { name: 'majd' });
 
 // insert one into a colliction
 const insertOneInto = (colName, obj) => {
   const client = new MongoClient(mongoURI, { useNewUrlParser: true });
-  client.connect(err => {
+  client.connect((err) => {
     if (err) throw err;
-    const db = client.db("blooddonor");
-    db.collection(colName).insertOne(obj, erro => {
+    const db = client.db('blooddonor');
+    db.collection(colName).insertOne(obj, (erro) => {
       if (err) throw erro;
-      console.log("1 document inserted");
+      console.log('1 document inserted');
     });
     client.close();
   });
 };
 
-insertOneInto("users", { name: "majd" });
+insertOneInto('users', { name: 'majd' });
 
 // delete one into a colliction
 const deleteOneFrom = (colName, obj) => {
   const client = new MongoClient(mongoURI, { useNewUrlParser: true });
-  client.connect(err => {
+  client.connect((err) => {
     if (err) throw err;
-    const db = client.db("blooddonor");
-    db.collection(colName).deleteOne(obj, erro => {
+    const db = client.db('blooddonor');
+    db.collection(colName).deleteOne(obj, (erro) => {
       if (err) throw erro;
-      console.log("1 document deleted");
+      console.log('1 document deleted');
     });
     client.close();
   });
 };
 
-deleteOneFrom("users", { name: "majd" });
+deleteOneFrom('users', { name: 'majd' });
