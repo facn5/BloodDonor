be1d8b4a18dde12374dcdd8a28c7a77fc18005de
majdya <majd.ya.1995@gmail.com>
2019-05-13 15:02:12 +0300

    setting up the database on atlas, adding some basic crud opirations Related #14

---
+++ mongodb.js
@@ -0,0 +1,72 @@
+// CRUD
+
+const { MongoClient } = require('mongodb');
+
+// eslint-disable-next-line no-multi-spaces
+const { mongoURI } = require('./keys_dev');
+
+// find all in a colletion
+const findAllIn = (colName, query) => {
+  const client = new MongoClient(mongoURI, { useNewUrlParser: true });
+  client.connect((err) => {
+    if (err) throw err;
+    const db = client.db('blooddonor');
+    db.collection(colName)
+      .find(query)
+      .toArray((error, result) => {
+        if (error) throw error;
+        console.log(result);
+      });
+    client.close();
+  });
+};
+
+findAllIn('users', { name: 'majd' });
+
+// find one in a colletion
+const findOneIn = (colName, query) => {
+  const client = new MongoClient(mongoURI, { useNewUrlParser: true });
+  client.connect((err) => {
+    if (err) throw err;
+    const col = client.db('blooddonor');
+    col.collection(colName).findOne(query, (error, res) => {
+      if (error) console.log(error);
+      console.log(res);
+    });
+    client.close();
+  });
+};
+
+findOneIn('users', { name: 'majd' });
+
+// insert one into a colliction
+const insertOneInto = (colName, obj) => {
+  const client = new MongoClient(mongoURI, { useNewUrlParser: true });
+  client.connect((err) => {
+    if (err) throw err;
+    const db = client.db('blooddonor');
+    db.collection(colName).insertOne(obj, (erro) => {
+      if (err) throw erro;
+      console.log('1 document inserted');
+    });
+    client.close();
+  });
+};
+
+insertOneInto('users', { name: 'majd' });
+
+// delete one into a colliction
+const deleteOneFrom = (colName, obj) => {
+  const client = new MongoClient(mongoURI, { useNewUrlParser: true });
+  client.connect((err) => {
+    if (err) throw err;
+    const db = client.db('blooddonor');
+    db.collection(colName).deleteOne(obj, (erro) => {
+      if (err) throw erro;
+      console.log('1 document deleted');
+    });
+    client.close();
+  });
+};
+
+deleteOneFrom('users', { name: 'majd' });
