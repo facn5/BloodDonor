fb7b79fcd182e89acb33564981a3125aef0cef30
karam1ashqar <karam.ashqar01@gmail.com>
2019-05-13 10:44:20 +0300

    Added express server, runs on 4000 port, has route of / and handles 404 and 500 server errors Relates #12

---
+++ server/app.js
@@ -0,0 +1,26 @@
+
+const express = require('express');
+const path = require('path');
+
+
+const favicon = require('serve-favicon');
+const bodyParser = require('body-parser');
+const compression = require('compression');
+
+const controllers = require('./controllers/router');
+
+const app = express();
+
+app.disable('x-powered-by');
+app.use(compression());
+// app.use(favicon(path.join(__dirname, '..', 'public', 'assets', 'favicon.ico')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '..', '..', 'public'), { maxAge: '30d' }));
+app.use(express.urlencoded())
+
+app.use(controllers);
+app.set('port', process.env.PORT || 4000);
+
+
+module.exports = app;
