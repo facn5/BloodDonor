8f8023a2a04e6771fc5d4d4a61ed28bdfb37b906
karam1ashqar <karam.ashqar01@gmail.com>
2019-05-13 11:22:24 +0300

    Removed commented lines, and body parser

--- server/app.js
+++ server/app.js
@@ -1,21 +1,12 @@
-
 const express = require('express');
 const path = require('path');
-
-
-const favicon = require('serve-favicon');
-const bodyParser = require('body-parser');
 const compression = require('compression');
-
 const controllers = require('./controllers/router');
-
 const app = express();
 
 app.disable('x-powered-by');
 app.use(compression());
-// app.use(favicon(path.join(__dirname, '..', 'public', 'assets', 'favicon.ico')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', '..', 'public'), { maxAge: '30d' }));
 app.use(express.urlencoded())
 
