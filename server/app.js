ae4c5be3bc1561fed8740ff6de74679ae1fe926c
karam1ashqar <karam.ashqar01@gmail.com>
2019-05-13 16:20:36 +0300

    Static reading dist

--- server/app.js
+++ server/app.js
@@ -8,11 +8,11 @@ const app = express();
 app.disable('x-powered-by');
 app.use(compression());
 app.use(express.json());
+app.use(express.static('../client/dist'));
 app.use(express.static(path.join(__dirname, '..', '..', 'public'), { maxAge: '30d' }));
-app.use(express.urlencoded());
 
 app.use(controllers);
-app.set('port', process.env.PORT || 4000);
+app.set('port', process.env.PORT || 4013);
 
 
 module.exports = app;
