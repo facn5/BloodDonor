9997839cbec1ccbd393fc7b1be38c9d31bd041d7
Tamer Nasser <tamernasser@outlook.com>
2019-05-15 13:53:12 +0300

    Added components to debug it

--- client/src/app.js
+++ client/src/app.js
@@ -1,4 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-ReactDOM.render(<div>hello world</div>, document.getElementById('root'));
+import Card from './components/card/card.js'
+import {Header} from './components/header/header.js'
+ReactDOM.render(<div>
+  <Header/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  <Card /></div>, document.getElementById('root'));
