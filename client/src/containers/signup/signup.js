3faed971a0168d6741e888fabdd99834f6953824
karam1ashqar <karam.ashqar01@gmail.com>
2019-05-16 14:59:03 +0300

    Added signup container with signup.css for styling

---
+++ client/src/containers/signup/signup.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import "./signup.css";
+
+const Signup = () => {
+  return (
+    <div className="signupContainer">
+      <h1 className="center">Sign up</h1>
+      <form method="POST" action="/signup">
+        <input name="username" placeholder="Username" type="text" />
+        <input
+          autoComplete="true"
+          name="password"
+          placeholder="Password"
+          type="password"
+        />
+        <input
+          autoComplete="true"
+          placeholder="Retype password"
+          type="password"
+        />
+        <input name="phoneNumber" placeholder="Phone number" type="tel" />
+        <button type="submit" name="submit">
+          Signup!
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
