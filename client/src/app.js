52b57be6456be565e798d99629107ac5c75b4397
obayda <obayda7h@gmail.com>
2019-05-16 14:27:21 +0300

    routes handling related #39

--- client/src/app.js
+++ client/src/app.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Card from './components/card/card.js'
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Link, Route, NavLink, Redirect, Prompt } from 'react-router-dom';
+import { Header } from './components/header/header'
+import Card from './components/card/card'
+import Login from './components/login/login'
+import Signup from './components/signup/signup'
+import Inspiration from './components/inspiration/inspiration'
+import Regform from './components/regform/regform'
+import About from './components/about/about'
 
-ReactDOM.render(<div>
-  <Card />
-  <Card />
-  <Card />
-  <Card />
-  <Card />
-  <Card />
-  <Card />
-  <Card />
-  <Card /></div>, document.getElementById('root'));
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Route component={Header} />
+        <Route path='/' exact component={Card} />
+        <Route path='/login' exact component={Login} />
+        <Route path='/signup' exact component={Signup} />
+        <Route path='/inspiration' exact component={Inspiration} />
+        <Route path='/regform' exact component={Regform} />
+        <Route path='/about' exact component={About} />
+      </Router>
+    );
+  }
+}
