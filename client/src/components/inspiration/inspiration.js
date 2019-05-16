8a4a38f8b5a4a9b64c4202aa1d6a035306bd7a66
obayda <obayda7h@gmail.com>
2019-05-16 14:32:18 +0300

    inspiration component initalized related #39

---
+++ client/src/components/inspiration/inspiration.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Inspiration extends Component {
+    render() {
+        return (<div>hello from Inspiration</div>);
+    }
+}
+
+export default Inspiration;
