be9d175da8b03fd2d383a6503538a092615fb653
karam1ashqar <karam.ashqar01@gmail.com>
2019-05-19 15:55:43 +0300

    Added authentication file for signup and login post requests and added function for signup with checking if the user already exists in the database relates to #46

---
+++ server/controllers/authentication.js
@@ -0,0 +1,43 @@
+const database = require('../database/mongodb');
+const utils = require('../utils');
+
+exports.signup = (body, res) => {
+  database.findOneIn(
+    'users',
+    { username: body.username },
+    (checkErr, result) => {
+      if (checkErr) {
+        res.json({ success: false, result: 'Please try again later!' });
+      }
+      if (result == null) {
+        utils.hash(body.password, (uTerr, hashedPassword) => {
+          if (uTerr) {
+            res.json({ success: false, result: 'Please try again later!' });
+          } else {
+            database.insertOneInto(
+              'users',
+              {
+                username: body.username,
+                password: hashedPassword,
+                phoneNumber: body.phoneNumber
+              },
+              (insertErr, success) => {
+                if (insertErr || !success) {
+                  res.json({
+                    success: false,
+                    result: 'Please try again later!'
+                  });
+                } else {
+                  res.json({
+                    success: true,
+                    result: 'Signed up successfully!'
+                  });
+                }
+              }
+            );
+          }
+        });
+      } else res.json({ success: false, result: 'User already exists!' });
+    }
+  );
+};
