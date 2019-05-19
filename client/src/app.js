d5e13f2b988cd0521f2bb92af095530caa030a50
Tamer Nasser <tamernasser@outlook.com>
2019-05-19 21:52:12 +0300

    Fixed Inspiration page component typo for route related #44

--- client/src/app.js
+++ client/src/app.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Header } from "./components/header/header";
 import Login from "./components/login/login";
 import Signup from "./containers/signup/signup";
-import { Inspiration } from "./components/inspiration/inspiration";
+import { InspirationPage } from "./components/inspirationPage/inspirationPage";
 import Regform from "./components/regform/regform";
 import About from "./containers/about/about";
 import Main from "./components/main.js";
@@ -15,7 +15,7 @@ const app = () => {
       <Route path="/" exact component={Main} />
       <Route path="/login" exact component={Login} />
       <Route path="/signup" exact component={Signup} />
-      <Route path="/inspiration" exact component={Inspiration} />
+      <Route path="/inspiration" exact component={InspirationPage} />
       <Route path="/regform" exact component={Regform} />
       <Route path="/about" exact component={About} />
     </Router>
