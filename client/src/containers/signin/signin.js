2c734ad4c7d598a37a3b07986ce30285c6d65409
karam1ashqar <karam.ashqar01@gmail.com>
2019-05-19 17:41:34 +0300

    typo errors and fixes as converting == to === relates to #46

--- client/src/containers/signin/signin.js
+++ client/src/containers/signin/signin.js
@@ -1,28 +1,70 @@
-import React from "react";
-import "./signin.css";
+import React, { Component } from 'react';
+import './signin.css';
 
-const Signin = props => (
-  <div className="signInContainer">
-    <h1 className="center">Sign in</h1>
+class Signin extends Component {
+  state = {
+    username: '',
+    password: '',
+    dbResult: '',
+    success: ''
+  };
+  handleChange = event => {
+    if (event.target.name === 'username')
+      this.setState({ username: event.target.value });
+    else if (event.target.name === 'password')
+      this.setState({ password: event.target.value });
+  };
+  handleSubmit = () => {
+    fetch('/signin', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: this.state.username,
+        password: this.state.password
+      })
+    })
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ dbResult: data.result });
 
-    <form className="signInForm" method="POST" action="/signin">
-      <input name="username" type="text" placeholder="username" />
-      <input
-        autoComplete="off"
-        name="password"
-        type="password"
-        placeholder="password"
-      />
+        if (data.success) this.setState({ success: 'green' });
+        else this.setState({ success: 'red' });
+      })
+      .catch(err => console.log(err));
+  };
+  render() {
+    return (
+      <div className="signInContainer">
+        <h1 className="center">Sign in</h1>
 
-      <button className="center" type="submit">
-        Sign in!
-      </button>
-    </form>
+        <input
+          value={this.state.username}
+          onChange={this.handleChange}
+          name="username"
+          type="text"
+          placeholder="username"
+        />
+        <input
+          autoComplete="off"
+          value={this.state.password}
+          onChange={this.handleChange}
+          name="password"
+          type="password"
+          placeholder="password"
+        />
 
-    <h3 className="center" onClick={() => props.history.push("/signup")}>
-      Sign up
-    </h3>
-  </div>
-);
+        <button onClick={this.handleSubmit} className="center" type="submit">
+          Sign in!
+        </button>
+        <p style={{ color: this.state.success }}>{this.state.dbResult}</p>
+        <h3 className="center" onClick={() => props.history.push('/signup')}>
+          Sign up
+        </h3>
+      </div>
+    );
+  }
+}
 
 export default Signin;
