ba151ebf713caf46816fb752c81e49b2913a9940
Tamer Nasser <tamernasser@outlook.com>
2019-05-19 23:11:03 +0300

    Changed component to class to add state related #44

--- client/src/components/inspirationPage/inspiration/inspiration.js
+++ client/src/components/inspirationPage/inspiration/inspiration.js
@@ -2,21 +2,34 @@ import React from 'react';
 import './inspiration.css';
 import  {InspCardDetails}  from "./inspCard/inspCard.js";
 
-export const Inspiration = ({ title, imgSrc, desc }) => (
-  <>
-    <div className='container-inspCard'>
-     <div className='container-card'>
+export class Inspiration extends React.Component {
+state = {
+  opened:false
+}
+render(){
+  const {
+    title,
+imgSrc,
+desc
+} = this.props;
+  return(
+     <>
+      <div className='container-inspCard'>
+       <div className={this.state.opened?'container-card container-card--opened':'container-card'}>
+          <div>
+            <img className="img" src={imgSrc} />
+          </div>
+          <div>
+            <p className="title">title : {title}</p>
+            <p className="shortDescription">desc: {desc}</p>
+            <button onClick={()=>{this.setState({opened:!this.state.opened})}}>Read more...</button>
+          </div>
+       </div>
         <div>
-          <img className="img" src={imgSrc} />
+            <InspCardDetails isVisible={this.state.opened} description={desc}/>
         </div>
-        <div>
-          <p className="title">title : {title}</p>
-          <p className="shortDescription">desc: {desc}</p>
-        </div>
-     </div>
-      <div>
-          <InspCardDetails imgSrc={imgSrc}  description={desc}/>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+}
+}
