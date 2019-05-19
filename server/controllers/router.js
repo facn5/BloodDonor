c5d6fbecae661f960ac30f2ec3b4da632467ced9
karam1ashqar <karam.ashqar01@gmail.com>
2019-05-19 15:57:07 +0300

    Added post request handling for route /signup relates to #46

--- server/controllers/router.js
+++ server/controllers/router.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const path = require('path');
 const cards = require('./cards.js');
-
+const authentication = require('./authentication');
 
 const router = express.Router();
 
 router.get('/getCards', cards.get);
 
 router.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', '..', 'client', 'dist', 'index.html'));
+  res.sendFile(
+    path.join(__dirname, '..', '..', 'client', 'dist', 'index.html')
+  );
 });
 
+router.post('/signup1', ({ body }, res) => {
+  authentication.signup(body, res);
+});
 
 module.exports = router;
