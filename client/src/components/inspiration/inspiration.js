4be147f4b8c31bbe00615b8ff2d04960706e1c36
Tamer Nasser <tamernasser@outlook.com>
2019-05-19 16:56:48 +0300

    Imported InspCardDetails related #44

--- client/src/components/inspiration/inspiration.js
+++ client/src/components/inspiration/inspiration.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import './style.css';
+import  {InspCardDetails}  from "./inspCard/inspCard.js";
 
 export const Inspiration = ({ title, imgSrc, desc }) => (
   <>
     <div className='container-inspCard'>
+    // first row
+     <div className='container-card'>
+        <div>
+          <img className="img" src={imgSrc} />
+        </div>
+        <div>
+          <p className="title">{title}</p>
+          <p className="shortDescription">{disc}</p>
+        </div>
+     </div>
+    // second row
       <div>
-        <p className="title">{title}</p>
-        <p className="shortDescription">{disc}</p>
-      </div>
-      <div>
-        <img className="img" src={imgSrc} />
+          <InspCardDetails imgSrc={imgSrc}  description={desc}/>
       </div>
     </div>
   </>
