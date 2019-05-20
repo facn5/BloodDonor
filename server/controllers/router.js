d77d6dc6a0271cb44832d79010844530ddbbff3c
Karam Ashqar <karam.ashqar01@gmail.com>
2019-05-20 14:57:37 +0300

    linter fix

--- server/controllers/router.js
+++ server/controllers/router.js
@@ -1,31 +1,31 @@
-const express = require("express");
-const path = require("path");
-const cards = require("./cards.js");
-const authentication = require("./authentication");
+const express = require('express');
+const path = require('path');
+const cards = require('./cards.js');
+const authentication = require('./authentication');
 
 const router = express.Router();
 
-router.get("/getCards", cards.get);
+router.get('/getCards', cards.get);
 
-router.get("/checkauth", (req, res) => {
+router.get('/checkauth', (req, res) => {
   authentication.checkCookies(req, res);
 });
 
-router.get("*", (req, res) => {
+router.get('*', (req, res) => {
   res.sendFile(
-    path.join(__dirname, "..", "..", "client", "dist", "index.html")
+    path.join(__dirname, '..', '..', 'client', 'dist', 'index.html'),
   );
 });
 
-router.post("/signup", ({ body }, res) => {
+router.post('/signup', ({ body }, res) => {
   authentication.signup(body, res);
 });
 
-router.post("/signin", ({ body }, res) => {
+router.post('/signin', ({ body }, res) => {
   authentication.signin(body, res);
 });
 
-router.post("/signout", (req, res) => {
+router.post('/signout', (req, res) => {
   authentication.logout(req, res);
 });
 
