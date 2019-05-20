3669df8030e3262f6692540c55b782b6f011eb80
Karam Ashqar <karam.ashqar01@gmail.com>
2019-05-20 16:04:56 +0300

    eslint fix

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,7 +1,7 @@
 const { sign, verify } = require('jsonwebtoken');
-const ppcookie = require("cookie");
-const database = require("../database/mongodb");
-const utils = require("../utils");
+const ppcookie = require('cookie');
+const database = require('../database/mongodb');
+const utils = require('../utils');
 const { SECRET } = require('../../keys_dev');
 
 exports.signup = ({ username, password, phoneNumber }, res) => {
@@ -16,61 +16,56 @@ exports.signup = ({ username, password, phoneNumber }, res) => {
         && password.length >= 6
         && username.length >= 3
       ) {
-        database.findOneIn(
-          'users',
-          { username },
-          (checkErr, result) => {
-            if (checkErr) {
-              res.json({
-                success: false,
-                result: 'Please try again later! db',
-              });
-              console.log(checkErr);
-              return;
-            }
-            if (result === null || result === undefined) {
-              utils.hash(password, (utilErr, hashedPassword) => {
-                if (utilErr) {
-                  res.json({
-                    success: false,
-                    result: "Please try again later!"
-                  });
-                  
-                } else {
-                  database.insertOneInto(
-                    "users",
-                    {
-                      username,
-                      password: hashedPassword,
-                      phoneNumber
-                    },
-                    (insertErr, success) => {
-                      if (insertErr || !success) {
-                        res.json({
-                          success: false,
-                          result: "Please try again later! db1"
-                        });
-                      } else {
-                        const userDetails = {
-                          u$u: username
-                        };
-                        const cookie = sign(userDetails, SECRET);
+        database.findOneIn('users', { username }, (checkErr, result) => {
+          if (checkErr) {
+            res.json({
+              success: false,
+              result: 'Please try again later! db',
+            });
+            console.log(checkErr);
+            return;
+          }
+          if (result === null || result === undefined) {
+            utils.hash(password, (utilErr, hashedPassword) => {
+              if (utilErr) {
+                res.json({
+                  success: false,
+                  result: 'Please try again later!',
+                });
+              } else {
+                database.insertOneInto(
+                  'users',
+                  {
+                    username,
+                    password: hashedPassword,
+                    phoneNumber,
+                  },
+                  (insertErr, success) => {
+                    if (insertErr || !success) {
+                      res.json({
+                        success: false,
+                        result: 'Please try again later! db1',
+                      });
+                    } else {
+                      const userDetails = {
+                        u$u: username,
+                      };
+                      const cookie = sign(userDetails, SECRET);
 
-                        res.cookie("jwt", cookie, {
-                          httpOnly: true
-                        });
-                        res.json({
-                          success: true,
-                          result: "Signed up successfully!"
-                        });
-                      }
+                      res.cookie('jwt', cookie, {
+                        httpOnly: true,
+                      });
+                      res.json({
+                        success: true,
+                        result: 'Signed up successfully!',
+                      });
                     }
-                  );
-                }
-              });
-            } else res.json({ success: false, result: 'User already exists!' });
-          },
-        );
+                  },
+                );
+              }
+            });
+          } else res.json({ success: false, result: 'User already exists!' });
+        });
       }
     }
   }
@@ -86,7 +81,7 @@ exports.signin = ({ username, password }, res) => {
     } else if (result === undefined || result === null) {
       res.json({
         success: false,
-        result: "Username doesn't exist!",
+        result: 'Username doesn\'t exist!',
       });
     } else {
       utils.compare(password, result.password, (utError, success) => {
