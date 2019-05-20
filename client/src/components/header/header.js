d1852489e36aba83a3bc26865fb2f121a78cf075
Karam Ashqar <karam.ashqar01@gmail.com>
2019-05-20 14:36:15 +0300

    Added checkauthentication function that is called whenever the user click on navbar and componentdidmount relates to #46

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -31,6 +31,20 @@ export class Header extends React.Component {
     this.checkAuth();
   };
 
+  checkAuth = () => {
+    fetch("/checkauth")
+      .then(res => res.json())
+      .then(data => {
+        if (data.authenticated) {
+          if (this.state.authenticated !== "Log out")
+            this.setState({ authenticated: "Log out" });
+        } else {
+          if (this.state.authenticated !== "Login")
+            this.setState({ authenticated: "Login" });
+        }
+      });
+  };
+
   exitNavbar = () => {
     this.setState({
       slideClass: "nav-links",
