7e6ee6e23d6e610692d0e237a932b92143c39e0b
obayda <obayda7h@gmail.com>
2019-05-20 17:44:10 +0300

    regestir form initialized related #58

--- client/src/components/regform/regform.js
+++ client/src/components/regform/regform.js
@@ -1,5 +1,97 @@
 import React, { Component } from "react";
+import './styles'
 
-export const Regform = () => {
-  return <div>Hello from Regform</div>;
-};
+class Regform extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      radio1: false
+    }
+    this.onRadioChange = this.onRadioChange.bind(this)
+  }
+
+  onRadioChange(e) {
+    console.log(e.target.value);
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="questionCard">
+          <input type="radio"
+            name="radio1"
+            value="yes"
+            checked={this.state.radio1 === "yes"}
+            onChange={this.onRadioChange} /> Yes
+
+        <input type="radio"
+            name="radio1"
+            value="no"
+            checked={this.state.radio1 === "no"}
+            onChange={this.onRadioChange} /> No
+          </div>
+
+        <div className="questionCard">
+          <input type="radio"
+            name="radio1"
+            value="yes"
+            checked={this.state.radio1 === "yes"}
+            onChange={this.onRadioChange} /> Yes
+
+        <input type="radio"
+            name="radio1"
+            value="no"
+            checked={this.state.radio1 === "no"}
+            onChange={this.onRadioChange} /> No
+          </div>
+
+        <div className="questionCard">
+          <input type="radio"
+            name="radio1"
+            value="yes"
+            checked={this.state.radio1 === "yes"}
+            onChange={this.onRadioChange} /> Yes
+
+        <input type="radio"
+            name="radio1"
+            value="no"
+            checked={this.state.radio1 === "no"}
+            onChange={this.onRadioChange} /> No
+          </div>
+
+        <div className="questionCard">
+          <input type="radio"
+            name="radio1"
+            value="yes"
+            checked={this.state.radio1 === "yes"}
+            onChange={this.onRadioChange} /> Yes
+
+        <input type="radio"
+            name="radio1"
+            value="no"
+            checked={this.state.radio1 === "no"}
+            onChange={this.onRadioChange} /> No
+          </div>
+
+        <div className="questionCard">
+          <input type="radio"
+            name="radio1"
+            value="yes"
+            checked={this.state.radio1 === "yes"}
+            onChange={this.onRadioChange} /> Yes
+
+        <input type="radio"
+            name="radio1"
+            value="no"
+            checked={this.state.radio1 === "no"}
+            onChange={this.onRadioChange} /> No
+          </div>
+      </div>
+    );
+  }
+}
+
+export default Regform;
