b921edd2fb1093c13f8bb28e0ce2c648184cffe2
Tamer Nasser <tamernasser@outlook.com>
2019-05-21 19:58:43 +0300

    Fixed auth bug

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -34,9 +34,10 @@ export class Header extends React.Component {
     fetch('/checkauth')
       .then(res => res.json())
       .then(data => {
+        if(data.authenticated){
         if( this.state.authenticated !== 'Log out')
           this.setState({ authenticated: 'Log out' });
-        else {
+        } else {
           if (this.state.authenticated !== 'Login')
             this.setState({ authenticated: 'Login' });
         }
