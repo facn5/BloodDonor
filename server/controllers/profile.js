7702cd1f7338a87872955853febf1eef3dab0da6
Tamer Nasser <tamernasser@outlook.com>
2019-05-21 10:19:25 +0300

    Deleted password from response related #56

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -2,6 +2,7 @@ const query = require('../database/mongodb.js');
 
 exports.get = (req, res) => {
   query.findOneIn('users', { username: req.params.username }, (err, results) => {
+    delete results.password;
     res.json({ data: results });
   });
 };
