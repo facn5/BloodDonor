c9d30c2d1f239930549fbf87903b0dff645806dc
Tamer Nasser <tamernasser@outlook.com>
2019-05-22 13:19:53 +0300

    fixed controller for update config related #58 related #72 related #69" git s

--- server/controllers/userConfig.js
+++ server/controllers/userConfig.js
@@ -1,25 +1,18 @@
 const database = require('../database/mongodb');
 
 exports.post = (
-  {
-    username,
-    bloodType,
-    pValidAge,
-    pHealthStatus,
-    pRecentSurgery,
-    pGetNotification,
-  },
+  obj,
   res,
 ) => {
   const config = {
-    bloodType,
-    pValidAge,
-    pHealthStatus,
-    pRecentSurgery,
-    pGetNotification,
+    bloodType: obj.bloodType,
+    pValidAge: obj.validAge || obj.pValidAge,
+    pHealthStatus: obj.goodHealth || obj.pHealthStatus,
+    pRecentSurgery: obj.recentSurgery || obj.pRecentSurgery,
+    pGetNotification: obj.notification || obj.pGetNotification,
   };
-  database.findOneAndUpdateUser(username, { config }, (err, results) => {
-    console.log('username', username);
+  database.findOneAndUpdateUser(obj.username, { config }, (err, results) => {
+    console.log('username', obj.username);
     if (err) console.log(err);
     res.json({ results });
   });
