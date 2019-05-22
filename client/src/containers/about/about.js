9f643128063a7a4e652fa221c2bd25bccbfdecac
obayda <obayda7h@gmail.com>
2019-05-22 17:49:51 +0300

    fixed typo

--- client/src/containers/about/about.js
+++ client/src/containers/about/about.js
@@ -19,21 +19,24 @@ const About = () => {
         <div className="cardDetails">
           <div className="subContainer">
             <h3 className="center">Terms of use</h3>
-            <p>Is to reach more and more good people like you so we can save
-              more people lives. Mother’s tears cannot save her child’s life but
-              your blood can.
-          </p>
+            <ul>
+              <li>✦ Ensure that any details which you supply to us to register for such access are accurate.</li>
+              <li>✦ To keep any personal login name and any password confidential.</li>
+              <li>✦ That we reserve the right to terminate your access to the Service should we consider that your use of the service is detrimental to the Service or to other users.</li>
+            </ul>
           </div>
         </div>
       </div>
       <div className="teamMembersSection">
-        <p>Team:-</p>
         <div className="cardDetails">
-          <div className="teamMembersSubContainer">
-            <TeamMember name="Tamer" photo="https://avatars2.githubusercontent.com/u/14230328?s=400&v=4" />
-            <TeamMember name="Obaydah" photo="https://avatars3.githubusercontent.com/u/6875484?s=400&v=4" />
-            <TeamMember name="Majd" photo="https://avatars0.githubusercontent.com/u/33071567?s=400&v=4" />
-            <TeamMember name="Karam" photo="https://avatars3.githubusercontent.com/u/46317630?s=400&v=4" />
+          <div className="subContainer">
+            <p className="center">Team</p>
+            <div className="teamMembersSubContainer">
+              <TeamMember name="Tamer" photo="https://avatars2.githubusercontent.com/u/14230328?s=400&v=4" />
+              <TeamMember name="Obaydah" photo="https://avatars3.githubusercontent.com/u/6875484?s=400&v=4" />
+              <TeamMember name="Majd" photo="https://avatars0.githubusercontent.com/u/33071567?s=400&v=4" />
+              <TeamMember name="Karam" photo="https://avatars3.githubusercontent.com/u/46317630?s=400&v=4" />
+            </div>
           </div>
         </div>
       </div>
